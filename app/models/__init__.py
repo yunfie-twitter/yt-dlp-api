753d90a0ccb27567ba7e22d7875ca179a6a53e90
ゆんふぃ <yunfie.twitter+3@gmail.com>
2026-01-25 12:10:07 +0900

    refactor: explicit exports in app/models/__init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,5 @@
-# Models package initialization
+from .request import InfoRequest, VideoRequest
+from .response import VideoInfo, SearchResponse
+from .internal import DownloadIntent
+
+__all__ = ["InfoRequest", "VideoRequest", "VideoInfo", "SearchResponse", "DownloadIntent"]
